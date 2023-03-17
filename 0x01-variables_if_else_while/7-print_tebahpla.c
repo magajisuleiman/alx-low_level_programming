3d9acf4c715a8cc21041f00f5c5247afc9846c06
magajisuleiman <magajisuleima1203@gmail.com>
2023-03-17 11:25:23 -0700

    7. Smile in the mirror

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+* main - For loop to print alphabet from a to z
+*
+* Return: value will be 0 if sucessful
+*/
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = 'z'; alphabet >= 'a'; alphabet--)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
