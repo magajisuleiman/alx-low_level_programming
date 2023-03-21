50eb5f700dab49e316493a5807edf7317bd2f73e
magajisuleiman <magajisuleima1203@gmail.com>
2023-03-21 09:59:14 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * main - print_putchar and with a line at end
+ *
+ * Return: 0 if successful
+*/
+
+int main(void)
+{
+
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
+
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+This directory contails all the files for the project carrried out during the alx SE programm. 
+
+The read me is still being modified. 
