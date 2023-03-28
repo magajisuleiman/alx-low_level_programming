6942f75b4436f8c52f2980ceab02e0781d0b531f
magajisuleiman <magajisuleima1203@gmail.com>
2023-03-27 20:29:54 -0700

    4. I can only go one way. I've not got a reverse gear

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
+void print_rev(char *s)
+{
+	int len = 0;
+	int i;
+
+	while (*s != '\n')
+	{
+		len++;
+		s++;
+	}
+	s--;
+	for (i = len; i > 0; i++)
+	{
+		_putchar(*s);
+		s--;
+	}
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -6,6 +6,7 @@
 		void swap_int(int *a, int *b);
 		void _puts(char *str);
 		int _strlen(char *s);
+		void print_rev(char *s);
 
 	#endif
 
