fa47a0c10216ca23a2a5b48eaafe38d936543303
magajisuleiman <magajisuleima1203@gmail.com>
2023-03-27 18:30:21 -0700

    0. 98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - function takes int and update to 98
+ * @n: pointer to an integer
+ *
+*/
+
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+This folder contains all the filesand solutions to the ALX 0x05. C - Pointers, arrays and strings projects. 
