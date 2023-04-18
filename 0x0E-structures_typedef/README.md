511d828bc13ae90a98e750a2260d11055c4726f0
magajisuleiman <magajisuleima1203@gmail.com>
2023-04-17 19:30:18 -0700

    0. Poppy

---
+++ 0x0E-structures_typedef/README.md
@@ -0,0 +1,2 @@
+This directory contains all the tsk solutions to alx 0x0E. C - Structures, typedef
+
---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,19 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * srtuct dog - user define data type sruct dog
+ * @name: structure menmber 1
+ * @age: structure menmber 2
+ * @owner: structure menmber 3
+ *
+ * Description: data type classify a dog with all its properties 
+*/
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif
