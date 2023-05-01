eae60034d1ec410473c583e093c2fd79f4950663
magajisuleiman <magajisuleiman1203@gmail.com>
2023-04-30 23:59:55 -0700

    1. List length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * listint_len - function that prints all the elements of a listint_t list.
+ * @h: refrence head of linked list
+ *
+ *
+ * Return: print out element ot the linke list.
+*/
+size_t listint_len(const listint_t *h)
+{
+	/* create a *temp pointer to head */
+	const listint_t *temp;
+	unsigned int element = 0;
+
+	temp = h;
+
+	/* use temp to traverse the ll and print data */
+	while (temp != NULL)
+	{
+		element++;
+		temp = (*temp).next;
+	}
+	return (element);
+}
