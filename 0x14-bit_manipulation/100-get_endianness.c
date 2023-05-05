567977446d97fbd9e93acd225ec83030ca46b9bf
magajisuleiman <magajisuleiman1203@gmail.com>
2023-05-04 19:26:39 -0700

    7. Crackme3

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * get_endianness - checks if a machine is little or big endian
+ * Return: 0 for big, 1 for little
+ */
+int get_endianness(void)
+{
+	unsigned int j;
+	char *c;
+
+	j = 1;
+	c = (char *) &j;
+
+	return (*c);
+}
+
