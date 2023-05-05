3a53f9ecbd4704bef6a687d0f420a99b6ad03f57
magajisuleiman <magajisuleiman1203@gmail.com>
2023-05-04 19:13:14 -0700

    solved task four and task 5

---
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+ * flip_bits - counts the number of bits to change
+ * to get from one number to another
+ * @n: first number
+ * @m: second number
+ *
+ * Return: number of bits to change
+ */
+unsigned int flip_bits(unsigned long int n, unsigned long int m)
+{
+	int j;
+	int index;
+	unsigned long int present;
+	unsigned long int xor;
+
+	xor = n ^ m;
+	index = 0;
+	j = 63;
+
+	while (j > 0)
+	{
+		present = xor >> j;
+		if (present & 1)
+			index++;
+		j--;
+	}
+
+	return (index);
+}
+
