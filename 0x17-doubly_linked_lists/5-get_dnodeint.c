ce073d47a1e5e3db4e2e795979ea7bbaedd0d1ff
magajisuleiman <magajisuleiman1203@gmail.com>
2023-06-15 18:26:50 -0700

    5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,5 +1,13 @@
 #include "lists.h"
 
+/**
+ *get_dnodeint_at_index - function that returns the nth node of a linked list
+ *@head: head node of linked list
+ *@index: position ofnode to e return
+ *
+ *Return: Null if list is empy and note at index
+ *
+*/
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int  counter = 0;
@@ -7,10 +15,8 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	while (head != NULL)
 	{
 		if (counter == index)
-		{
 			return (head);
-		}
-		head->next = head;
+		head  = head->next;
 		counter++;
 	}
 	return (NULL);
