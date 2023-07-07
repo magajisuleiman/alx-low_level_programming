794fbfb8708d1d50a7118782c8add15b430e1d90
magajisuleiman <magajisuleiman1203@gmail.com>
2023-07-07 02:24:32 -0700

    0. >>> ht = {}

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,38 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_create - Creates a hash table
+ * @size: The size of the array
+ *
+ * Return: A pointer to the newly created hash table
+ */
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	hash_table_t *hash_table = NULL;
+	unsigned long int i;
+
+	/* Allocate memory for the hash table structure */
+	hash_table = malloc(sizeof(hash_table_t));
+	if (hash_table == NULL)
+		return (NULL); /* Failed to allocate memory */
+
+	/* Allocate memory for the array of hash_node_t pointers */
+	hash_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (hash_table->array == NULL)
+	{
+		free(hash_table);
+		return (NULL); /* Failed to allocate memory */
+	}
+
+	/* Initialize each element in the array to NULL */
+	for (i = 0; i < size; i++)
+	{
+		hash_table->array[i] = NULL;
+	}
+
+	/* Set the size of the hash table */
+	hash_table->size = size;
+
+	return (hash_table);
+}
+
---
+++ 0x1A-hash_tables/README.md
@@ -0,0 +1,1 @@
+This directory contains all the solutions for ALX project 0x1A. C - Hash tables
